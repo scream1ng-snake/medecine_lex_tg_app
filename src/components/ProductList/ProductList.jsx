f425f55081b8c51b1e8e45e58ededcf534376572
lykasov_am <lykasov_am@lexema.ru>
2023-05-21 20:25:20 +0500

    FIX: warning

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -62,6 +62,7 @@ const ProductList = () => {
     <div className="list">
       {products.map((item) => (
         <ProductItem
+          key={item.id}
           product={item} 
           onAdd={onAdd}
           className='item'
