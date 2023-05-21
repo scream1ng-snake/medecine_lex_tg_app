3d33168f2106b53ec886bdf183892a387d40c562
lykasov_am <lykasov_am@lexema.ru>
2023-05-21 14:03:32 +0500

    FIX

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import Header from './components/Header/Header';
+// import Header from './components/Header/Header';
 import useTelega from './hooks/UseTelegram';
 
 function App() {
@@ -8,6 +8,7 @@ function App() {
   
   React.useEffect(() => {
     tg.ready()
+    // eslint-disable-next-line
   }, [])
    return (
     <div className="App">
